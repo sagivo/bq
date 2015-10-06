62f603ddbe609e80be6a7d8687983f03e49312a9
Sagiv Ofek <sagiv4@gmail.com>
2015-10-06 16:18:04 -0400

    First commit

---
+++ app/src/main/java/com/example/sagivo/myapplication/HomeFragment.java
@@ -0,0 +1,20 @@
+package com.example.sagivo.myapplication;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by sagivo on 10/6/15.
+ */
+public class HomeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_home ,container,false);
+        return v;
+    }
+}
---
+++ app/src/main/java/com/example/sagivo/myapplication/ProfileFragment.java
@@ -0,0 +1,17 @@
+package com.example.sagivo.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ProfileFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_profile ,container,false);
+        return v;
+    }
+}
