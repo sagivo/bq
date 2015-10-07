d1cbd024697446c8ea2d43f9ab2c6343bc151b72
Sagiv Ofek <sagiv4@gmail.com>
2015-10-07 12:09:28 -0400

    Animation

--- app/src/main/java/com/example/sagivo/myapplication/Store.java
+++ app/src/main/java/com/example/sagivo/myapplication/Store.java
@@ -14,13 +14,25 @@ public class Store{
         return new Store[]{
                 new Store("East Village", "store1", "80 2nd ave", true),
                 new Store("West Village", "store2", "80 2nd ave", true),
-                new Store("Washington Sq. Park", "store3", "80 2nd ave", true),
+                new Store("Washington Sq. Park", "store3", "80 2nd ave", false),
                 new Store("Midtown West", "store4", "80 2nd ave", true),
                 new Store("2nd Ave.", "store5", "80 2nd ave", true),
                 new Store("Broadway", "store6", "80 2nd ave", true),
                 new Store("Chinatown", "store1", "80 2nd ave", true),
                 new Store("5th street", "store2", "80 2nd ave", true),
                 new Store("Downtown", "store3", "80 2nd ave", true),
-                new Store("Times Sq.", "store4", "7th Ave", true) };
+                new Store("Times Sq.", "store4", "7th Ave", true),
+
+                new Store("East Village", "store1", "80 2nd ave", true),
+                new Store("West Village", "store2", "80 2nd ave", true),
+                new Store("Washington Sq. Park", "store3", "80 2nd ave", false),
+                new Store("Midtown West", "store4", "80 2nd ave", true),
+                new Store("2nd Ave.", "store5", "80 2nd ave", true),
+                new Store("Broadway", "store6", "80 2nd ave", true),
+                new Store("Chinatown", "store1", "80 2nd ave", true),
+                new Store("5th street", "store2", "80 2nd ave", true),
+                new Store("Downtown", "store3", "80 2nd ave", true),
+                new Store("Times Sq.", "store4", "7th Ave", true)
+        };
     }
 }
