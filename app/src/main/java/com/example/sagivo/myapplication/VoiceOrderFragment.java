b1a3257a05a87056de30ccaab81f1371fe1da2fe
Sagiv Ofek <sagiv4@gmail.com>
2015-10-07 22:59:52 -0400

    Add Speech

--- app/src/main/java/com/example/sagivo/myapplication/VoiceOrderFragment.java
+++ app/src/main/java/com/example/sagivo/myapplication/VoiceOrderFragment.java
@@ -9,15 +9,19 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.List;
 
 public class VoiceOrderFragment extends Fragment {
 
     private static final int SPEECH_REQUEST_CODE = 10;
+    TextView text_view;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_map ,container,false);
+        View v = inflater.inflate(R.layout.fragment_voice_order ,container,false);
+
+        text_view = (TextView)v.findViewById(R.id.voice_result);
         recordVoice();
         return v;
     }
@@ -26,11 +30,10 @@ public class VoiceOrderFragment extends Fragment {
     // This is where you process the intent and extract the speech text from the intent.
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == SPEECH_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
             List<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
             String spokenText = results.get(0);
-            Log.w("aaa", spokenText);
+            text_view.setText(spokenText.toString());
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
@@ -38,6 +41,7 @@ public class VoiceOrderFragment extends Fragment {
     void recordVoice(){
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Place Your Order...");
         // Start the activity, the intent will be populated with the speech text
         startActivityForResult(intent, SPEECH_REQUEST_CODE);
     }
